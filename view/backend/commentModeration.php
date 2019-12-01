ddb82ecb3f07fdc0bf29d38344347cacd9589f31
Jérôme POCHET <blackmath62@gmail.com>
2019-12-01 13:41:09 +0100

    test comment validation for commentview

--- view/backend/commentModeration.php
+++ view/backend/commentModeration.php
@@ -2,27 +2,19 @@
 <div class="container ">
 <section class="page-section">
     <!-- Blog Post -->
-    <?php
-    while ($postblog = $blogmodel->fetch()) {
-        $title = $postblog['post_title'];
-        $datepost = $postblog['post_date'];
-        $postmessage = $postblog['post_content'];
-        $postnumber = $postblog['post_id'];
-        $postuser = $postblog['mail'];
-        ?>
-    <div class="text-center" id="<?=$postnumber?>">
+    <div class="text-center" id="">
             <!-- <img class="card-img-top" src="public/img/oc.png" alt="Card image cap"> -->
 
             <div class="card-body">
-                <h2 class="card-title btn-xl btn-primary"><?= $title ?></h2>
-                <p class="card-text"><?= substr($postmessage, 0, 200).'...' ?></p>
-                <a href="index.php?action=longPost&id=<?=$postnumber?>" class="btn btn-primary">Lire plus ! &rarr;</a>
+                <h2 class="card-title btn-xl btn-primary">Titre</h2>
+                <p class="card-text"></p>
+                <a href="index.php?action=longPost&id=...." class="btn btn-primary">Lire plus ! &rarr;</a>
             </div>
             <div class="card-footer text-muted">
-                Posté le <?= $datepost ?> par <?= $postuser ?>
+                Posté le ..... par .....
             </div>
         </div>
-    <?php } ?>
+
 </section>
 </div>
 <?php
