01db67747ae3b7cf60f9042a0ecca25f5cd7c3ea
Jérôme POCHET <blackmath62@gmail.com>
2020-04-16 19:33:15 +0200

    change naming function and add new request

--- Model/BlogManager.php
+++ Model/BlogManager.php
@@ -8,8 +8,8 @@ class BlogManager extends Manager // la classe CommentManager hérite de Manager
     public function lastPost() // affichage des 3 derniers chapos pour la page d'accueil
     {
         $db = $this->dbConnect(); // la base de donnée de l'objet courant
-        $lastPostResult = $db->query("SELECT post_id, post_title, post_date,post_chapo, post_content, users_id, users.mail, users.Pseudo FROM users INNER JOIN post_list USING(users_id)ORDER BY post_date DESC LIMIT 3");
-        $getlastPostResult = $lastPostResult->fetchAll(\PDO::FETCH_CLASS,'Blog');
+        $lastPostResult = $db->query("SELECT post_id, post_title, post_date,post_chapo, post_content, users_id,users.Pseudo, users.mail, users.Pseudo FROM users INNER JOIN post_list USING(users_id)ORDER BY post_date DESC LIMIT 3");
+        $getlastPostResult = $lastPostResult->fetchAll(\PDO::FETCH_OBJ);
         return $getlastPostResult;
     }
 
@@ -17,16 +17,28 @@ class BlogManager extends Manager // la classe CommentManager hérite de Manager
     {
         $db = $this->dbConnect(); // la base de donnée de l'objet courant
         $allPostResult = $db->query("SELECT post_id, post_title, post_date,post_chapo, post_content, users_id, users.mail, users.Pseudo, modification_date FROM users INNER JOIN post_list USING(users_id)ORDER BY post_date DESC");
-        $getAllPostResult = $allPostResult->fetchAll(\PDO::FETCH_CLASS,'Blog');
+        $getAllPostResult = $allPostResult->fetchAll(\PDO::FETCH_OBJ);
         return $getAllPostResult;
+    }
+    public function allComment() // affichage de tous les chapos des posts
+    {
+        $db = $this->dbConnect(); // la base de donnée de l'objet courant
+        $allCommentResult = $db->query("SELECT comment_id, comment_title, comment_date, comment_content, users_id, users.Pseudo, treatment_date,validate_id, validation_label 
+        FROM users 
+        INNER JOIN comment USING(users_id)
+        INNER JOIN comment_validation USING(validate_id)
+        ORDER BY comment_date DESC");
+        $getAllCommentResult = $allCommentResult->fetchAll(\PDO::FETCH_OBJ);
+        return $getAllCommentResult;
     }
 
     public function postComment($postnumber) // affichage pour l'utilisateur des commentaires validés
     {
         $db = $this->dbConnect(); // la base de donnée de l'objet courant
-        $lastComment = $db->prepare("SELECT comment_id, comment_title, comment_date, comment_content, users_id, users.mail, validate_id FROM users INNER JOIN COMMENT USING(users_id) where post_id = ? AND validate_id = 2  ORDER BY comment_date LIMIT 6");
+        $lastComment = $db->prepare("SELECT comment_id, comment_title, comment_date, comment_content, users_id, users.Pseudo, users.mail, validate_id FROM users INNER JOIN COMMENT USING(users_id) where post_id = ? AND validate_id = 2  ORDER BY comment_date LIMIT 6");
         $lastComment->execute(array($postnumber));
-        return $lastComment;
+        $listComment = $lastComment->fetchAll(\PDO::FETCH_OBJ);
+        return $listComment;
     }
     public function getChangePost($postnumber)
     {
@@ -39,9 +51,9 @@ class BlogManager extends Manager // la classe CommentManager hérite de Manager
     public function getLongPost($postnumber)
     {
         $db = $this->dbConnect(); // la base de donnée de l'objet courant
-        $longPostResult = $db->prepare("SELECT post_id, post_title, post_date,post_chapo, post_content, users_id, users.mail, modification_date FROM users INNER JOIN post_list USING(users_id) WHERE post_id = ?");
+        $longPostResult = $db->prepare("SELECT post_id, post_title, post_date,post_chapo,users.Pseudo, post_content, users_id, users.mail, modification_date FROM users INNER JOIN post_list USING(users_id) WHERE post_id = ?");
         $longPostResult->execute(array($postnumber));
-        $postResult = $longPostResult->fetchObject('Blog');
+        $postResult = $longPostResult->fetch(\PDO::FETCH_OBJ);
         return $postResult;
     }
 
--- Model/CommentManager.php
+++ Model/CommentManager.php
@@ -5,62 +5,13 @@ namespace memberSpace\Model;
 require_once("Model/Manager.php");
 class CommentManager extends Manager // la classe CommentManager hérite de Manager
 {
-    public function commentReport($usersId, $postnumber, $commentId)
+    public function commentReport()
     {
         $db = $this->dbConnect(); // la base de donnée de l'objet courant
-        $addReport = $db->prepare("INSERT INTO report_comment (users_id, comment_id, post_id) VALUES (?,?,?)");
-        $addReport->execute(array($usersId, $commentId, $postnumber));
-        return $addReport;
+        $commentId = $_GET['id'];
+        $validateId = $_GET['modification'];
+        $changeStatusComment = $db->prepare('UPDATE comment SET validate_id = :validateId WHERE comment_id = :commentId'); // on prépare l'insertion dans la BDD
+        $changeStatus = $changeStatusComment->execute(array('validateId' => $validateId, 'commentId' => $commentId)); // On insere dans la BDD 
+        return $changeStatus;
     }
-    public function numberCommentReport(){
-        $db = $this->dbConnect(); // la base de donnée de l'objet courant
-        $numberReport = $db->prepare("SELECT post_id from report_comment");
-        $numberReport->execute(array());
-        return $numberReport;
-    }
-    public function numberCommentWait(){
-        $db = $this->dbConnect(); // la base de donnée de l'objet courant
-        $numberWait = $db->prepare("SELECT post_id from comment WHERE validate_id = 1");
-        $numberWait->execute(array());
-        return $numberWait;
-    }
-    public function checkUserReport($usersId, $postnumber){ // Vu utilisateur, pour les commentaires qu'il a signalé
-        $db = $this->dbConnect(); // la base de donnée de l'objet courant
-        $checkUserReport = $db->prepare("SELECT comment_id from report_comment WHERE users_id = ? AND post_id = ? ORDER BY comment_id");
-        $checkUserReport->execute(array($usersId, $postnumber));
-        return $checkUserReport;
-    }
-    public function checkAllReport($postnumber){ // Liste de tous les signalements de commentaires
-        $db = $this->dbConnect(); // la base de donnée de l'objet courant
-        $checkAllReport = $db->prepare("SELECT comment_id from report_comment WHERE post_id = ? ORDER BY comment_id");
-        $checkAllReport->execute(array($postnumber));
-        return $checkAllReport;
-    }
-    public function removeReport($usersId, $postnumber, $commentId){
-        $db = $this->dbConnect(); // la base de donnée de l'objet courant
-        $deleteReport = $db->prepare("DELETE from report_comment WHERE users_id = ? AND post_id = ? AND comment_id = ?");
-        $deleteReport->execute(array($usersId, $postnumber, $commentId));
-        return $deleteReport;
-    }
-    
-    // todo voir si cette fonction est utile
-    public function commentInfo($postnumber){
-        $db = $this->dbConnect(); // la base de donnée de l'objet courant
-        $allComment = $db->prepare("SELECT comment.comment_id, comment.comment_title, comment.comment_date, comment.comment_content,
-        comment.post_id, comment.users_id, comment.validate_id, comment.treatment_date,
-        CASE 
-        WHEN comment.comment_id = report_comment.comment_id THEN COUNT(report_comment.comment_id)
-        END AS numberReport
-        FROM comment
-        INNER JOIN report_comment ON report_comment.post_id = comment.post_id
-        WHERE comment.post_id = ?
-        GROUP BY comment.comment_id, comment.comment_title, comment.comment_date, comment.comment_content,
-        comment.post_id, comment.users_id, comment.validate_id, comment.treatment_date
-        ORDER BY comment_date");
-        $allComment->execute(array($postnumber));
-        $getAllComment = $allComment->fetchAll(\PDO::FETCH_CLASS,'Comment');
-        return $getAllComment;
-    }
-    
-
 }
