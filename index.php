2a66018287f78f4b22ab8811bc48662aca9bf0f5
Jérôme POCHET <blackmath62@gmail.com>
2020-07-02 15:58:59 +0200

    delete display error

--- index.php
+++ index.php
@@ -16,13 +16,6 @@ $request = new Request();
 $controllerFront = new ControllerFrontend();
 $controllerBackend = new ControllerBackend();
 
-error_reporting(E_ALL);
-ini_set("display_errors", 1); // Afficher plus d'erreur a retirer pour la mise en prod
-
-// le fichier index, il s'agit du routeur qui va chercher les informations en fonction des données
-// ce fichier renomme correctement la barre d'adresse
-// a le même effet qu'include, c'est à dire pour ramener une page mais à l'avantage de ne rien renvoyer en cas d'erreur
-
 $action = $request->get('action');
 if (isset($action)) {
         switch ($action) {
